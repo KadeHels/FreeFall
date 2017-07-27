6f0778a15ad371a2ea651b398d943f9f9875db44
Kade H <kadeiszman@gmail.com>
2017-07-26 20:53:02 -0500

    cleaned up and impliment
    Cleaned up html and implemented the angularjs code to allow calculations.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 var app = angular.module("myApp", []);
 
 app.controller("myCtrl", function($scope) {
-    $scope.distance = "-1";
-    $scope.gravity = "9.8";
-    $scope.time = "8";
+    $scope.distance = -1;
+    $scope.gravity = 9.8;
+    $scope.time = 8;
+    $scope.calculateDistance = function($scope){
+    	this.distance = .5 * this.gravity * this.time * this.time;
+    };
+    $scope.calculateTime = function($scope){
+    	this.time = Math.sqrt(2 * this.distance / this.gravity);
+    };
+    $scope.calculateGravity = function($scope){
+    	this.gravity = 2 * this.distance / this.time / this.time;
+    };
+
 });
 
 
